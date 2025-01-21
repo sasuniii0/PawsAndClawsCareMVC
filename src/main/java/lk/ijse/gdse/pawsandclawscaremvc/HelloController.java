fb131edf2a0a5b8317ca1feca507a9068f38c949
sasuniii0 <sasuniwijerathne@gmail.com>
2025-01-21 17:28:12 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse/pawsandclawscaremvc/HelloController.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.pawsandclawscaremvc;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.gdse.pawsandclawscaremvc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.gdse.pawsandclawscaremvc to javafx.fxml;
+    exports lk.ijse.gdse.pawsandclawscaremvc;
+}
